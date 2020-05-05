6bdde113d4f4f40202442919d9341e922a266f69
Пользователь <chrispsow@gmail.com>
2020-05-05 16:24:05 +0500

    pagination

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,7 @@ def gen_slug(s):
 
 class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=150, blank=True, unique=True)
+    slug = models.SlugField(max_length=200, blank=True, unique=True)
     body = models.TextField(blank=True, db_index=True)
     tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateTimeField(auto_now_add=True)
@@ -33,14 +33,17 @@ class Post(models.Model):
         return reverse('post_delete_url', kwargs={'slug': self.slug})
 
     def save(self, *args, **kwargs):
-        if not self.id:
+        if not self.id or not self.slug:
             self.slug = gen_slug(self.title)
         super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['-date_pub']
 
 
 class Tag(models.Model):
     title = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=70, blank=True, unique=True)
 
     def __str__(self):
         return self.title
@@ -56,3 +59,11 @@ class Tag(models.Model):
 
     def get_delete_url(self):
         return reverse('tag_delete_url', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.id or not self.slug:
+            self.slug = slugify(self.title, allow_unicode=True)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['title']
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 from .models import Post, Tag
 from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectUpdateMixin, ObjectDeleteMixin
@@ -8,7 +9,21 @@ from .forms import PostForm, TagForm
 
 def posts_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/index.html', context={'posts': posts})
+    paginator = Paginator(posts, 4)
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    is_paginated = page.has_other_pages()
+    prev_url = f'?page={page.previous_page_number()}' if page.has_previous() else None
+    next_url = f'?page={page.next_page_number()}' if page.has_next() else None
+
+    context = {
+        'page': page,
+        'is_paginated': is_paginated,
+        'prev_url': prev_url,
+        'next_url': next_url
+    }
+    return render(request, 'blog/index.html', context)
 
 
 class PostDetail(ObjectDetailMixin, View):
