11f8128128736966891d9b40e370b94f1ce46ad9
Пользователь <chrispsow@gmail.com>
2020-05-05 16:43:21 +0500

    simple search

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 from .models import Post, Tag
 from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectUpdateMixin, ObjectDeleteMixin
 from .forms import PostForm, TagForm
 
 def posts_list(request):
-    posts = Post.objects.all()
+    search_query = request.GET.get('search', '')
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
     paginator = Paginator(posts, 4)
     page_number = request.GET.get('page', 1)
     page = paginator.get_page(page_number)
